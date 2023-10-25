22f4b9d878047d7a03244dabcbbd3afa7812d573
fayruza117 <fbobomurodova073@gmail.com>
2023-10-25 05:44:45 +0300

    homework is done

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// Type conversation
+//string, boolean => Number
+//NaN => Not a Number
+//let a = "1234..56";
+//Number() constructor function
+//console.log(parseInt(a));
+//console.log(parseFloat(a));
+// console.log(+a);
+// console.log(a * 1);
+// console.log(a / 1);
+// console.log(a - 0);
+// console.log(3 + a);
+ 
+// number, boolean => string
+// let a = 123.45;
+
+// String() constructor function
+//console.log(String(a));
+//console.log(a + "");
+
+//Number => String
+//console.log(a.toString());
+//console.log(a.toFixed(5));
+
+// Operators
+// Arithmatic
+// +, -, *, /, %, ++, --, **, //, =>
+
+//console.log(10 % 3);
+//let a = 1;
+//console.log(a);
+//console.log(++a);
+//console.log(a);
+//console.log(--a);
+//console.log(a);
+//console.log(2 ** 8);
+//console.log(2 // 4);
+
+// Comparison
+// > < <= >= == === != !==
+
+//console.log(1 > 2);
+//console.log(1 < 2);
+//console.log(1 < 2);
+//console.log(1 < 2);
+
+let age = prompt("Yoshingiz nechida");
+alert(2023 - age + ' yilda tugilgansiz');
+alert( Number(age) + 3 );
+let istak = prompt("Nechi yil yashashni hohlaysiz?");
+let hisob = alert(istak - age + " " + "yil umriz qoldi");
+
